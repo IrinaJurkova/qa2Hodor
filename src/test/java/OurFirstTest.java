28c567ff1749d902c474cd4f48eff6e396aee159
IrinaJurkova <59800743+IrinaJurkova@users.noreply.github.com>
2020-02-16 19:26:41 +0200

    Delete OurFirstTest.java

--- src/test/java/OurFirstTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class OurFirstTest {
-    @Test
-    public void firstTest() {
-               System.out.println("Hello world!");
-               System.out.println();
-    }
-}
